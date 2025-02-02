e4453b456e91b8948f0c1b6d5dc84616c2a6c16a
Aleksandra Parshina <coyote7.p@gmail.com>
2025-02-02 16:01:12 +0500

    00: настройка junit/ delete dependency selenium

--- app-testing/src/main/java/kz/ticketon/BaseClassAppTest.java
+++ app-testing/src/main/java/kz/ticketon/BaseClassAppTest.java
@@ -8,25 +8,25 @@ import java.net.URL;
 
 
 public class BaseClassAppTest extends BaseClassTest {
-    AndroidDriver driver = null;
-
-
-    public void initialize() throws MalformedURLException {
-        DesiredCapabilities capabilities = new DesiredCapabilities();
-
-        capabilities.setCapability("platformName", "Android");
-        capabilities.setCapability("deviceName", "sdk_gphone64_x86_64");
-        capabilities.setCapability("platformVersion", "15");
-        capabilities.setCapability("automationName", "Appium");
-        capabilities.setCapability("appPackage", "com.google.android.apps.nexuslauncher");
-        capabilities.setCapability("appActivity", "com.google.android.apps.nexuslauncher.NexusLauncherActivity");
-
-
-        try {
-            URL url = new URL("http://127.0.0.1:4723/wd/hub");
-            driver = new AndroidDriver(url, capabilities);
-        } catch (MalformedURLException e) {
-            System.out.println(e.getMessage());
-        }
-    }
+//    AndroidDriver driver = null;
+//
+//
+//    public void initialize() throws MalformedURLException {
+//        DesiredCapabilities capabilities = new DesiredCapabilities();
+//
+//        capabilities.setCapability("platformName", "Android");
+//        capabilities.setCapability("deviceName", "sdk_gphone64_x86_64");
+//        capabilities.setCapability("platformVersion", "15");
+//        capabilities.setCapability("automationName", "Appium");
+//        capabilities.setCapability("appPackage", "com.google.android.apps.nexuslauncher");
+//        capabilities.setCapability("appActivity", "com.google.android.apps.nexuslauncher.NexusLauncherActivity");
+//
+//
+//        try {
+//            URL url = new URL("http://127.0.0.1:4723/wd/hub");
+//            driver = new AndroidDriver(url, capabilities);
+//        } catch (MalformedURLException e) {
+//            System.out.println(e.getMessage());
+//        }
+//    }
 }
--- app-testing/src/test/java/kz/ticketon/StartTest.java
+++ app-testing/src/test/java/kz/ticketon/StartTest.java
@@ -6,11 +6,11 @@ import java.net.MalformedURLException;
 
 public class StartTest extends BaseClassAppTest{
 
-    @Test
-    public void startTicketon() throws MalformedURLException {
-        initialize();
-
-//        $x("com.google.android.apps.nexuslauncher:id/overview_actions_view");
-     // $x("//android.widget.TextView[@content-desc='Predicted app: Ticketon']").click();
-    }
+//    @Test
+//    public void startTicketon() throws MalformedURLException {
+//        initialize();
+//
+////        $x("com.google.android.apps.nexuslauncher:id/overview_actions_view");
+//     // $x("//android.widget.TextView[@content-desc='Predicted app: Ticketon']").click();
+//    }
 }
